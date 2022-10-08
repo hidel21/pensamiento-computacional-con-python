a04a9e755a52b3758538a8d962b8a155c625490c
Hidel <hidelmartinez21@gmail.com>
2022-10-08 19:09:42 -0300

    subir esto

---
+++ pensamiento computacional/fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci(n-1)+fibonacci(n-2)
+
+
+n = int(input('Escribe un entero: '))
+print(f'El numero fibonacci para {n} es {fibonacci(n)}')
+
+
---
+++ pensamiento computacional/programas_ramificados.py
@@ -0,0 +1,12 @@
+num_1 = int(input("Introduce un numero: "))
+num_2 = int(input("Introduce otro numero: "))
+
+
+if num_1 > num_2:
+    print("El primer numero es mayor que el segundo")
+elif num_1 < num_2:
+    print("El segundo numero es mayor que el primero")
+else:
+    print("Ambos son iguales")
+
+    
